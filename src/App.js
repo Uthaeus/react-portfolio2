71f73335ee0872a01094d36e77e48db5de9ae566
Uthaeus <romanlavery@gmail.com>
2023-03-28 11:50:22 -0600

    installed react-router-dom and setup initial routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,38 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
+import AboutPage from "./pages/About";
+import ContactPage from "./pages/Contact";
+import HomePage from "./pages/Home";
+import Layout from "./pages/Layout";
+import ProjectsPage from "./pages/Projects";
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout />,
+    children: [
+      {
+        index: true,
+        element: <HomePage />
+      },
+      {
+        path: '/about',
+        element: <AboutPage />
+      },
+      {
+        path: '/contact',
+        element: <ContactPage />
+      },
+      {
+        path: 'projects',
+        element: <ProjectsPage />
+      }
+    ]
+  }
+]);
+
 function App() {
-  return (
-    <div>
-      <h2>Let's get started!</h2>
-    </div>
-  );
+  return <RouterProvider router={router} />
 }
 
 export default App;
